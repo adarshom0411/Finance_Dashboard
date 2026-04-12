30a7176272b86258a19ad8d175c6cc5623ce8c80
Adarsh Singh <singhadarsh.om7355@gmail.com>
2026-04-12 19:22:26 +0530

    fix: swagger request bodies added

--- myBackend/src/routes/index.js
+++ myBackend/src/routes/index.js
@@ -5,10 +5,12 @@ const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
 const financeRoutes = require("./financeRoutes");
 const dashboardRoutes = require("./dashboardRoutes");
+const recordRoutes = require("./recordRoutes"); // ✅ ADD THIS
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/finance", financeRoutes);
 router.use("/dashboard", dashboardRoutes);
+router.use("/records", recordRoutes); // ✅ ADD THIS
 
 module.exports = router;
